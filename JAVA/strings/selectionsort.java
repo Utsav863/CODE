c41def8b0d4331d1ca54a764da43c95ea1bfd600
Utsav863 <utsav2484.be22@chitkara.edu.in>
2023-04-16 00:31:50 +0530

    h

---
+++ JAVA/strings/selectionsort.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class selectiosort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            int smallest=i;
+            for (int j = i + 1; j < n; j++) {
+                if (a[smallest] > a[j]) {
+                    smallest=j;  
+            }
+        }
+            int temp = a[smallest];
+            a[smallest] = a[i];
+            a[i] = temp;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
